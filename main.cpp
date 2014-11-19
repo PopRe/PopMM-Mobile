7961fcb13b817ccae17114aa51dcd3d6dfac2b05
Dariusz Jania <dariusz.jania@gmail.com>
2014-11-19 11:00:05 +0100

    Changed formatting

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-	QCoreApplication::setOrganizationName("Populous Reincarnated");
-	QCoreApplication::setOrganizationDomain("http://www.popre.net/");
-	QCoreApplication::setApplicationName(QString("PopMMMobile"));
+    QCoreApplication::setOrganizationName("Populous Reincarnated");
+    QCoreApplication::setOrganizationDomain("http://www.popre.net/");
+    QCoreApplication::setApplicationName(QString("PopMMMobile"));
 
     Dialog w;
     w.showMaximized();
